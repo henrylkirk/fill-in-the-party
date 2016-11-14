343ded53dc2cc7ed1b4354691b276a133c00974b
Henry Kirk <henry@the-kirks.com>
2016-11-14 11:24:28 -0500

    Added link to 'contact' page

--- assets/includes/footer.inc.php
+++ assets/includes/footer.inc.php
@@ -1,5 +1,7 @@
     <footer class="container" style="min-height:200px; background-color:#009245;color:#fff;text-align:center;padding-top:50px;">
         ARM PANTS GAMES © 2016 
+        <br /><br />
+        <a href="" style="color:black;text-decoration: underline;">Contact Us</a>
     </footer>
 </div>
 </body>
